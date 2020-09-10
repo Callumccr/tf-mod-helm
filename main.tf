bb12e64302fa19cac251c30722433d5d4d7222c7
Callum <callumccr@gmail.com>
2020-09-10 19:40:36 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 resource "helm_release" "default" {
-  count      = var.enabled ? 1 : 0
-  name       = var.release_name
-  repository = var.chart_repository_url != "" ? var.chart_repository_url : ""
-  chart      = var.chart
-  devel      = var.devel
-  version    = var.chart_version
-  values     = var.values
+  count               = var.enabled ? 1 : 0
+  name                = var.release_name
+  repository          = var.chart_repository_url != "" ? var.chart_repository_url : ""
+  repository_username = var.username
+  repository_password = var.password
+  chart               = var.chart
+  devel               = var.devel
+  version             = var.chart_version
+  values              = var.values
 
   dynamic "set" {
     for_each = var.set
@@ -23,15 +25,13 @@ resource "helm_release" "default" {
     }
   }
 
-  namespace           = var.k8s_namespace
-  verify              = var.verify
-  keyring             = var.keyring
-  timeout             = var.timeout
-  disable_webhooks    = var.disable_webhooks
-  reuse_values        = var.reuse_values
-  force_update        = var.force_update
-  recreate_pods       = var.recreate_pods
-  wait                = var.wait
-  repository_username = var.username
-  repository_password = var.password
+  namespace        = var.k8s_namespace
+  verify           = var.verify
+  keyring          = var.keyring
+  timeout          = var.timeout
+  disable_webhooks = var.disable_webhooks
+  reuse_values     = var.reuse_values
+  force_update     = var.force_update
+  recreate_pods    = var.recreate_pods
+  wait             = var.wait
 }
